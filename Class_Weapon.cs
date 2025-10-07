24e707c4f6b210556b9dbdd65095cce72433f763
Max Bergqvist <max.bergqvist@student.nbi-handelsakademin.se>
2025-10-07 15:03:12 +0200

    Uploading Class_Weapon to dev branch

---
+++ Class_Weapon.cs
@@ -0,0 +1,13 @@
+namespace App;
+
+public class Weapon
+{
+    string WeaponName;
+    string WeaponDamage;
+
+    public Weapon(string weaponName, string weaponDamage)
+    {
+        WeaponName = weaponName;
+        WeaponDamage = weaponDamage;
+    }
+}
